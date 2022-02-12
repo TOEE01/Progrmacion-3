c9f3d6f31f17c0c04b9b6986af806ecf0cf36825
TOEE01 <69540536+TOEE01@users.noreply.github.com>
2022-02-11 20:00:59 -0600

    comentando el codigo
    codigo comentado

--- ejercicios_de_clase/src/main/java/com/mycompany/ejercicios_de_clase/NewClass.java
+++ ejercicios_de_clase/src/main/java/com/mycompany/ejercicios_de_clase/NewClass.java
@@ -12,26 +12,26 @@ import java.util.Scanner;
  */
 public class NewClass {
     public static void main(String[] args) {
-        int n,num,x,count;
-        Scanner sc = new Scanner(System.in);
-        num = 0;
+        int n,num,x,count;//aqui declaramos las variablea que uaremos
+        Scanner sc = new Scanner(System.in);//usamos el scanner para que lea lo ingresado por el usuario
+        num = 0;//lo empezamo asi para no halla conflictos depues
         System.out.println("Ingrese un numero entero");
-        n = sc.nextInt();
-        while(n>0){
-            num = num + 1;
+        n = sc.nextInt();// aqui le reacinamos un valor a la varible
+        while(n>0){//en el buble decimos que mientra n sea mayor a cero va ha hacer esto
+            num = num + 1;//incrementamos la variable
             
-            x=1;
-           count = 0;
+            x=1;//la declamos con un vamor icinial asi como variable de control
+           count = 0;// la declaramos asi para que no halla conflictos despues
            
-           while(x<=num){
-               if(num%x==0){
-                   count = count + 1;
+           while(x<=num){ // aqui condicionamos que x debe ser menor o igual a num y si eso se cumple va a hacer lo siguiente
+               if(num%x==0){// aqui decidimos si el numero en num es primo
+                   count = count + 1;//aqui count sera igual a el mismo mas 1 solo si se cumple la condicion de arriba
                }
-               x = x+1;
+               x = x+1;//aqui subimos la variable x para que no se quede en un ciclo infinito
            }
-           if(count == 2){
-               System.out.println("El numero " + num +" es primo");
-               n = n-1;
+           if(count == 2){ // aqui decimos si la variable count es igual a dos esto lo que euire decir es que si los numero ingesado solo tuvieon dos divisores y si los tuvo entonces es primo
+               System.out.println("El numero " + num +" es primo");//aqui escribimos en pantalla que es primo
+               n = n-1;//aqui la decrementamos siempre y cuando halla un numero primo
            }
         }
     }
